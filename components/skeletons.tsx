9e93f46a9539799c2a23a4709f0fab5be352f9e6
Yosef <yosefworku67@gmail.com>
2025-12-19 15:33:15 +0300

    updated loading skeletons for dark mode

--- components/skeletons.tsx
+++ components/skeletons.tsx
@@ -12,16 +12,16 @@ export function CardListSkeleton() {
 
 function CardSkeleton() {
   return (
-    <div className="flex flex-col justify-between gap-4 p-4 rounded-md shadow-none border bg-gray-100">
+    <div className="flex flex-col justify-between gap-4 p-4 rounded-md shadow-none border bg-gray-100 dark:bg-gray-800">
       <div className="flex flex-col gap-4 rounded-xl">
-        <Skeleton className="aspect-17/11 w-full bg-white rounded-xl" />
+        <Skeleton className="aspect-17/11 w-full bg-white dark:bg-gray-900 rounded-xl" />
         <div className="p-2">
-          <Skeleton className="h-18 bg-gray-200 rounded-xl " />
+          <Skeleton className="h-18 bg-gray-200 dark:bg-gray-900 rounded-xl " />
         </div>
       </div>
       <div className="flex flex-row justify-between gap-4 px-2 pb-2">
-        <Skeleton className="h-10 w-32 bg-gray-200 rounded-xl" />
-        <Skeleton className="h-10 w-32 bg-gray-200 rounded-xl" />
+        <Skeleton className="h-10 w-32 bg-gray-200 dark:bg-gray-900 rounded-xl" />
+        <Skeleton className="h-10 w-32 bg-gray-200 dark:bg-gray-900 rounded-xl" />
       </div>
     </div>
   )
@@ -40,17 +40,17 @@ export function SearchResultsListSkeleton() {
 function SearchResultSkeleton() {
   return (
     <div className="flex flex-col sm:flex-row p-0 gap-0 rounded-md shadow-none border ">
-      <div className="w-full sm:w-3/10 p-4 px-6 bg-gray-100">
-        <Skeleton className="aspect-17/10 bg-white" />
+      <div className="w-full sm:w-3/10 p-4 px-6 bg-gray-100 dark:bg-gray-900">
+        <Skeleton className="aspect-17/10 bg-white dark:bg-gray-800" />
       </div>
-      <div className="w-full sm:w-7/10 bg-gray-50 flex flex-col justify-between gap-4 p-4 py-6">
+      <div className="w-full sm:w-7/10 bg-gray-50 dark:bg-gray-800 flex flex-col justify-between gap-4 p-4 py-6">
         <div className="px-2">
-          <Skeleton className="h-18 bg-gray-200" />
+          <Skeleton className="h-18 bg-gray-200 dark:bg-gray-900" />
         </div>
 
         <div className="flex flex-row justify-between px-2 gap-3">
-          <Skeleton className="h-10 w-32 bg-gray-200" />
-          <Skeleton className="h-10 w-32 bg-gray-200" />
+          <Skeleton className="h-10 w-32 bg-gray-200 dark:bg-gray-900" />
+          <Skeleton className="h-10 w-32 bg-gray-200 dark:bg-gray-900" />
         </div>
       </div>
     </div>
